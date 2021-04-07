4058ae62b2324976ef0e85f9f841de9a5ad07d11
Mohot416 <Mohot416@users.noreply.github.com>
2021-04-07 16:05:00 +0800

    Solve the bug between barriers and Enemys
    barriers can be destroy by enemy in proper time

--- Assets/BarrierStatusController.cs
+++ Assets/BarrierStatusController.cs
@@ -2,8 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Enemy
+{
+    private int num;
+    public int Num
+    {
+        get { return num; }
+        set { num = value; }
+    }
+    private int atk;
+    public int Atk
+    {
+        get { return atk; }
+        set { atk = value; }
+    }
+    public Enemy(int inputAtk, int inputNum) //constructor
+    {
+        atk = inputAtk;
+        num = inputNum;
+    }
+}
+
 public class BarrierStatusController : MonoBehaviour
 {
+    private Enemy small = new Enemy(2, 0);
+    private Enemy middle = new Enemy(5, 0);     //3 types of enemys
+    private Enemy giant = new Enemy(10, 0);
+    private bool IsCoroutine = false;
     public int HealthPoints = 100;
     // Start is called before the first frame update
     void Start()
@@ -14,8 +39,41 @@ public class BarrierStatusController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        print(small.Num);
         if (HealthPoints <= 0)
+        {
             Destroy(gameObject, 0);
+            return;
+        }
+        if (IsCoroutine == false)   //防止同時重複過多Coroutine
+        {
+            if (small.Num > 0 || middle.Num > 0 || giant.Num > 0)
+            {
+                StartCoroutine(BeingAttacked(small, middle, giant));
+                IsCoroutine = true;
+            }
+        }
     }
 
+    IEnumerator BeingAttacked(Enemy small, Enemy middle, Enemy giant)
+    {
+        Debug.Log("CoroutineTest Start At" + Time.time);
+        //lose health
+        HealthPoints = HealthPoints - (small.Atk * small.Num) - (middle.Atk * middle.Num) - (giant.Atk * giant.Num);
+        yield return new WaitForSeconds(1f);
+        IsCoroutine = false;
+        Debug.Log("CoroutineTest End At" + Time.time);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Small_Enemy")
+            small.Num++;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Small_Enemy")
+            small.Num--;
+    }
 }
--- Assets/EnemySetting.cs
+++ Assets/EnemySetting.cs
@@ -10,10 +10,10 @@ public class EnemySetting : MonoBehaviour
     public int atk = 2; //敵人攻擊力，初始為2
     public int def = 0; //敵人防禦力，初始為0
     public int health = 100;    //敵人生命值，初始為100
+    public int firePower = 55;
 
     //敵人預設速度(常數)、當前速度(變數)
     private float initialSpeed, currnetSpeed = 1.0f;
-    private bool attacking = false;
     public float InitialSpeed
     {
         get { return initialSpeed; }
@@ -23,11 +23,6 @@ public class EnemySetting : MonoBehaviour
     {
         get { return currnetSpeed; }
         set { currnetSpeed = value; }
-    }
-    public bool Attacking
-    {
-        get { return attacking; }
-        set { attacking = value; }
     }
 
     private void Start()
@@ -51,22 +46,19 @@ public class EnemySetting : MonoBehaviour
     //接觸到barriers就停下
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        currnetSpeed = 0.0f;
         if (collision.tag == "Barriers")
-        {
-            attacking = true;
-        }
+            currnetSpeed = 0.0f;
     }
 
     //離開barriers就照原速度繼續前進
     private void OnTriggerExit2D(Collider2D collision)
     {
-        currnetSpeed = initialSpeed;
-        attacking = false;
+        if (collision.tag == "Barriers")
+            currnetSpeed = initialSpeed;
     }
 
     private void OnMouseDown()
     {
-        health = health - 80;
+        health = health - (firePower - def);
     }
 }
